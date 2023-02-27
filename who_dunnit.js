8d09284b5c34a3f8360be6f7b9a6417fcf1eed12
Chris Barclay <chris.barclay1@gmail.com>
2023-02-27 17:07:44 +0000

    Episode 1 - Correct. Episode 2 - Incorrect.

--- who_dunnit.js
+++ who_dunnit.js
@@ -1,17 +1,35 @@
 
 // EPISODE 1
-const scenario = {
-    murderer: 'Miss Scarlet',
-    room: 'Library',
-    weapon: 'Rope'
-  };
+// const scenario = {
+//     murderer: 'Miss Scarlet',
+//     room: 'Library',
+//     weapon: 'Rope'
+//   };
   
-  const declareMurderer = function() {
-    return `The murderer is ${scenario.murderer}.`;
-  }
+//   const declareMurderer = function() {
+//     return `The murderer is ${scenario.murderer}.`;
+//   }
   
-  const verdict = declareMurderer();
-  console.log(verdict);
+//   const verdict = declareMurderer();
+//   console.log(verdict);
 //  "The murderer is Miss Scarlet."
 //  CORRECT 
+
+// EPISODE 2
+const murderer = 'Professor Plum';
+
+const changeMurderer = function() {
+  murderer = 'Mrs. Peacock';
+}
+
+const declareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
+
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
+// "The murderer is Professor Plum."
+// WRONG
+// TypeError: Assignment to constant variable
 
