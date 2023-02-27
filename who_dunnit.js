12adcb0310ab696f507d89f3fe597cd4d3c6d5ec
Chris Barclay <chris.barclay1@gmail.com>
2023-02-27 17:10:58 +0000

    Episode 3 - Correct

--- who_dunnit.js
+++ who_dunnit.js
@@ -16,20 +16,38 @@
 //  CORRECT 
 
 // EPISODE 2
-const murderer = 'Professor Plum';
+// const murderer = 'Professor Plum';
 
-const changeMurderer = function() {
-  murderer = 'Mrs. Peacock';
-}
+// const changeMurderer = function() {
+//   murderer = 'Mrs. Peacock';
+// }
 
-const declareMurderer = function() {
-  return `The murderer is ${murderer}.`;
-}
+// const declareMurderer = function() {
+//   return `The murderer is ${murderer}.`;
+// }
 
-changeMurderer();
-const verdict = declareMurderer();
-console.log(verdict);
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
 // "The murderer is Professor Plum."
 // WRONG
 // TypeError: Assignment to constant variable
+
+
+// EPISODE 3
+let murderer = 'Professor Plum';
+
+const declareMurderer = function() {
+  let murderer = 'Mrs. Peacock';
+  return `The murderer is ${murderer}.`;
+}
+
+const firstVerdict = declareMurderer();
+console.log('First Verdict: ', firstVerdict);
+
+const secondVerdict = `The murderer is ${murderer}.`;
+console.log('Second Verdict: ', secondVerdict);
+// "First Verdict: The murderer is Mrs. Peacock."
+// "Second Verdict: The murderer is Professor Plum."
+// CORRECT
 
