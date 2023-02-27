62166ec904615b16e124f7c6d19673783a35e5f1
Chris Barclay <chris.barclay1@gmail.com>
2023-02-27 17:24:39 +0000

    Episode 5 - Correct. Comments added to episodes 1 - 4

--- who_dunnit.js
+++ who_dunnit.js
@@ -14,6 +14,7 @@
 //   console.log(verdict);
 //  "The murderer is Miss Scarlet."
 //  CORRECT 
+// Comment - There is only one variable set for murderer. 
 
 // EPISODE 2
 // const murderer = 'Professor Plum';
@@ -32,6 +33,7 @@
 // "The murderer is Professor Plum."
 // WRONG
 // TypeError: Assignment to constant variable
+// Comment - I thought that the code would execute along with the error for the reassigned const variable.
 
 
 // EPISODE 3
@@ -50,21 +52,50 @@
 // "First Verdict: The murderer is Mrs. Peacock."
 // "Second Verdict: The murderer is Professor Plum."
 // CORRECT
+// Comment - The let variable inside the function is prioritised over the let variables outside of the function when the function is called.
+// The let variable inside the function cannot be accessed by the secondVerdict variable.  
 
 
 // EPISODE 4
-let suspectOne = 'Miss Scarlet';
-let suspectTwo = 'Professor Plum';
-let suspectThree = 'Mrs. Peacock';
+// let suspectOne = 'Miss Scarlet';
+// let suspectTwo = 'Professor Plum';
+// let suspectThree = 'Mrs. Peacock';
 
-const declareAllSuspects = function() {
-  let suspectThree = 'Colonel Mustard';
-  return `The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`;
-}
+// const declareAllSuspects = function() {
+//   let suspectThree = 'Colonel Mustard';
+//   return `The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`;
+// }
 
-const suspects = declareAllSuspects();
-console.log(suspects);
-console.log(`Suspect three is ${suspectThree}.`);
+// const suspects = declareAllSuspects();
+// console.log(suspects);
+// console.log(`Suspect three is ${suspectThree}.`);
 // "The suspects are Miss Scarlet, Professor Plum, Colonel Mustard."
 // "Suspect three is Mrs. Peacock."
 // CORRECT
+// Comment - The same logic applies as in Episode 3. 
+
+
+// EPISODE 5
+const scenario = {
+    murderer: 'Miss Scarlet',
+    room: 'Kitchen',
+    weapon: 'Candle Stick'
+  };
+  
+  const changeWeapon = function(newWeapon) {
+    scenario.weapon = newWeapon;
+  }
+  
+  const declareWeapon = function() {
+    return `The weapon is the ${scenario.weapon}.`;
+  }
+  
+  changeWeapon('Revolver');
+  const verdict = declareWeapon();
+  console.log(verdict);
+// "The weapon is the Revolver."
+// Comment - The values within the object can be modified even though the scenario has been set as a constant variable. 
+// CORRECT
+
+
+
